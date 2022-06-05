b207dfc43cee166877b2775ca601df2a48201425
Patar Martua Doli Siahaan <38348364+patarshn@users.noreply.github.com>
2022-06-05 17:29:58 +0000

    update timestamp microservice api

--- index.js
+++ index.js
@@ -20,9 +20,25 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let date = req.params.date;
+  let utc = new Date(date);
+  let unix;
+  
+  if(!(utc > 0)){
+    utc = new Date(parseInt(date));
+  }
+  unix = Math.floor(utc.getTime())
+
+  let callback = {
+    unix: unix,
+    utc: utc.toGMTString()    
+  }
+
+  res.json(callback);
 });
+
+
 
 
 
